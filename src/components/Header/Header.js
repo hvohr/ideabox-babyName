40da1c1d39e9a8c4afa441e27399c375cca49819
Hollis Vohr <hollis.vohr@gmail.com>
2023-07-03 23:20:51 -0600

    fix: remove buttons from header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,10 @@ import './Header.css'
 function Header() {
   return (
     <header className='navBar'>
-      <button className="home-button">Home</button>
       <div className='title-section'>
         <img src={require("../images/suspect.png")}></img>
         <h1>New Identity Generator</h1>
       </div>
-      <button className="saved-container-button">Saved Identities</button>
     </header>
   )
 }
