41c031bdbe4f4b4368ee8c19d1d33ed21b1041fa
Hollis Vohr <hollis.vohr@gmail.com>
2023-07-03 21:03:32 -0600

    feat: add styling changes and save section buttons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,16 @@
 import './Header.css'
 
 function Header() {
-return (
-<header className='navBar'>
-      <img src={require ("../images/suspect.png")}></img>
-      <h1>New Identity Generator</h1>
+  return (
+    <header className='navBar'>
+      <button className="home-button">Home</button>
+      <div className='title-section'>
+        <img src={require("../images/suspect.png")}></img>
+        <h1>New Identity Generator</h1>
+      </div>
+      <button className="saved-container-button">Saved Identities</button>
     </header>
-)
+  )
 }
 
 export default Header
