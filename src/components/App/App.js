fefe5787df6580479fb50c7c76e34c99ddeaa617
Hollis Vohr <hollis.vohr@gmail.com>
2023-07-03 20:46:06 -0600

    feat: add new button, styling, and clean up app component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,21 +5,31 @@ import IdentityCard from '../IdentityCard/IdentityCard'
 
 
 function App() {
-const [identity, setIdentity] = useState([])
-function getIdentity() {
-  fetch(`https://randomuser.me/api/`)
-  .then(response => response.json())
-  .then(data => setIdentity(data.results))
-}
-useEffect(() => {
-  getIdentity()
-}, [])
-let userData = identity.map(user => <IdentityCard firstName={user.name.first} lastName={user.name.last} gender={user.gender} phone={user.phone} email={user.email} birthday={user.dob.date}/> )
- return (
+  const [identity, setIdentity] = useState([])
+  function intialLoad() {
+    if (identity.length === 0) {
+      return <h2>No new identity yet -- click the button above to generate!</h2>
+    }
+  }
+  function getIdentity() {
+    fetch(`https://randomuser.me/api/`)
+      .then(response => response.json())
+      .then(data => setIdentity(data.results))
+  }
+  let userData = identity.map(user => <IdentityCard
+    firstName={user.name.first}
+    lastName={user.name.last}
+    gender={user.gender}
+    phone={user.phone}
+    email={user.email}
+    birthday={user.dob.date} />
+  )
+  return (
     <div className='App'>
       <Header />
-      <main classname="main-container">
-        <button onClick={getIdentity}>Get New Identity</button>
+      <main className="main-container">
+        <button className="generate-button" onClick={getIdentity}>Get New Identity</button>
+        {!identity.length && <h2 className='empty-line'>In some trouble? Need some new personal information? Click the button above to generate!</h2>}
         {userData}
       </main>
     </div>
--- src/components/IdentityCard/IdentityCard.js
+++ src/components/IdentityCard/IdentityCard.js
@@ -2,6 +2,10 @@ import React from 'react'
 import "./IdentityCard.css"
 
 function IdentityCard(props) {
+  console.log(props)
+  if (props.length === 0) {
+    return <h2>No new identity yet -- click the button above to generate!</h2>
+  }
   return (
     <section className="card-container">
       <h2>{props.firstName} {props.lastName}</h2>
@@ -9,6 +13,7 @@ function IdentityCard(props) {
       <p>{props.phone}</p>
       <p>{props.gender}</p>
       <p>{props.email}</p>
+      <button className='save-button'>Save Identity</button>
     </section>
   )
 }
