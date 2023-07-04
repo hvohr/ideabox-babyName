04aa70fdf9061dc0c4b6adf7dbe9254dd3ccc348
Hollis Vohr <hollis.vohr@gmail.com>
2023-07-03 21:51:06 -0600

    fix: add id and birthplace to identity card

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,10 @@
 import './App.css';
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Header from '../Header/Header'
 import IdentityCard from '../IdentityCard/IdentityCard'
 
-
 function App() {
   const [identity, setIdentity] = useState([])
-  function intialLoad() {
-    if (identity.length === 0) {
-      return <h2>No new identity yet -- click the button above to generate!</h2>
-    }
-  }
   function getIdentity() {
     fetch(`https://randomuser.me/api/`)
       .then(response => response.json())
@@ -19,17 +13,19 @@ function App() {
   let userData = identity.map(user => <IdentityCard
     firstName={user.name.first}
     lastName={user.name.last}
-    gender={user.gender}
+    birthCity={user.location.city}
+    birthCountry={user.location.country}
     phone={user.phone}
     email={user.email}
-    birthday={user.dob.date} />
+    birthday={user.dob.date}
+    id={user.login.md5} />
   )
   return (
     <div className='App'>
       <Header />
       <main className="main-container">
         <button className="generate-button" onClick={getIdentity}>Get New Identity</button>
-        {!identity.length && <h2 className='empty-line'>In some trouble? Need some new personal information? Click the button above to generate!</h2>}
+        {!identity.length && <h2 className="empty-line">In some trouble? Need some new personal information? Click the button above to generate!</h2>}
         {userData}
       </main>
     </div>
