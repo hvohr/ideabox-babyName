94f03aabfb6d9da2fe366eec1bbdc3bd8ef5a298
Hollis Vohr <hollis.vohr@gmail.com>
2023-07-07 11:14:35 -0600

    fix: styling issues when switching to router

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,15 +4,16 @@ import { NavLink } from 'react-router-dom'
 function NavBar() {
   return (
     <nav className='navBar'>
-      <div className='title-section'>
+      <div className='nav-section'>
         <li className='home-button'>
           <NavLink to='/' className='home-class'>Home</NavLink>
         </li>
-      </div>
-      <div className='nav-section'>
         <li className='saved-section-button'>
           <NavLink to='/savedSection' className='saved-class'>Saved Identities</NavLink>
         </li>
+      </div>
+      <div className='title-section'>
+        <h1 className='title'>New Identity Generator</h1>
       </div>
     </nav>
   )
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,18 +2,14 @@ import React from 'react'
 import NavBar from '../NavBar/NavBar'
 
 function Home(props) {
-  console.log(props)
   return (
-    <div className='home-container'>
+    <div>
       <NavBar />
-      <main className='home-button'>
-        <h1 className='title'>New Identity Generator</h1>
-        <section className="main-container">
-          <button className="generate-button" onClick={props.getIdentity}>Get New Identity</button>
-          {!props.userData.length && <h2 className="empty-line">In some trouble? Need some new personal information? Click the button above to generate!</h2>}
-          {props.userData}
-        </section>
-      </main>
+      <section className='home-container'>
+        <button className="generate-button" onClick={props.getIdentity}>Get New Identity</button>
+        <h2 className="empty-line">In some trouble? Need some new personal information? Click the button above to generate!</h2>
+        {props.userData}
+      </section>
     </div>
   )
 }
